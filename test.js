065a4836d2e1f774cd57b7471441f726fef2816e
anoopelias <anoopelias@gmail.com>
2018-03-24 16:47:29 +0530

    Added basic puzzle8 implmentation

--- test.js
+++ test.js
@@ -1,34 +1,39 @@
 const fs = require('mz/fs');
 const EOL = require('os').EOL;
-const collinear = require('./collinear');
+const puzzle8 = require('./puzzle8');
 
 async function readInput(filename) {
-    const input = await fs.readFile(filename, 'utf8');
-    const lines = input.split(EOL);
-    const length = parseInt(lines.shift());
-    console.log('length', length);
-    let points = [];
+  const input = await fs.readFile(filename, 'utf8');
+  const lines = input.split(EOL);
+  const length = parseInt(lines.shift());
+  let board = [];
 
-    for (let i=0; i<length; i++) {
-        let line = lines[i];
-        let values = line.split(',').map(val => val.trim());
-        points.push({
-            x: parseFloat(values[0]),
-            y: parseFloat(values[1]),
-        })
+  for (let i=0; i<length; i++) {
+    let line = lines[i];
+    let values = line.split(' ')
+      .map(val => val.trim())
+      .filter(val => val)
+      .map(val => parseInt(val));
+    let row = [];
+
+    for (let j=0; j<length; j++) {
+      row.push(values[j]);
     }
-    return {
-        points: points,
-        length: length,
-    };
+
+    board.push(row);
+  }
+  return {
+    board: board,
+    length: length,
+  };
 }
 
 async function run() {
-    const filename = process.argv[2];
-    const input = await readInput(filename);
-    const output = collinear(input);
+  const filename = process.argv[2];
+  const input = await readInput(filename);
+  const output = puzzle8(input);
 
-    console.log(output);
+  console.log(output);
 }
 
 run();
