f30c0155e1ae0367c344407c3bb8c09844efd546
anoopelias <anoopelias@gmail.com>
2018-05-18 06:06:00 +0530

    Updated for leaderboard
    Signed-off-by: anoopelias <anoopelias@gmail.com>

--- index.js
+++ index.js
@@ -45,15 +45,34 @@ async function submit() {
     });
 }
 
+function leaderboard() {
+    request({
+        url: BASE + '/leaderboard',
+        qs: {
+            name: getGitName(),
+        },
+    }, function (error, response, body) {
+        if (error) {
+            console.error('Error: ', error.message);
+        } else {
+            console.log(body);
+        }
+    });
+}
+
 if (process.argv[2] === 'ping') {
     ping();
 } else if (process.argv[2] === 'submit') {
     submit();
+} else if (process.argv[2] === 'leaderboard') {
+    leaderboard();
 } else {
     if (process.argv[2]) {
         console.log('\nUnknown argument :', process.argv[2])
     }
-    console.log('\nUsage : \n    ping: To ping the server\n    submit: To submit request to the server\n');
+    console.log('\nUsage : \n    ping: To ping the server' +
+      '\n    submit: To submit request to the server\n' +
+      '\n    leaderboard: To view the current leaderboard\n');
 }
 
 
